32228c88843d79ca9063363f624dfdc6e41cf0ef
Madi <mayradiazchambilla327@gmail.com>
2025-06-13 12:11:33 -0500

    Avance del ejercicio 4, para el desarrollo de las raices en la formula general

---
+++ Problema4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main (){
+	
+	int PC,SC, TC;
+	cout<< "Ingrese el primer coeficiente" << endl;
+	cin>> PC;
+	cout<< "Ingrese el segundo coeficiente" << endl;
+	cin>> SC;
+	cout<< "Ingrese el tercer coeficiente" << endl;
+	cin>> TC;
+	Dis=pow(SC,2)-4*PC*TC;
+	X1= (-SC-sqrt(Dis))/2*a;
+	
+	cout<< "Las raices de la ecuacion son: "<< endl;
+}
