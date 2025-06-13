4cdd1273e30d55683390a6f07942397d8f86f5b2
Madi <mayradiazchambilla327@gmail.com>
2025-06-12 23:25:33 -0500

    Agregando mi primer ejercicio, haciendo uso de la libreria cmath para hallar el area del circulo

---
+++ Problema1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	int radio;
+	cout<< "Ingrese el radio del circulo" << endl;
+	cin >> radio;
+	cout<< "El area del circulo es: " << M_PI*pow(radio,2)<<endl;
+}
