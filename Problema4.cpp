e9a731db12b38999ed38b2a6ec3b7aca530dbbe2
Madi <mayradiazchambilla327@gmail.com>
2025-06-13 22:57:54 -0500

    Agregando el ejrcicio 4 version final, donde su funcion es encontrar raices ya sean reales iguales, diferentes o complejas

--- Problema4.cpp
+++ Problema4.cpp
@@ -4,15 +4,32 @@
 using namespace std;
 int main (){
 	
-	int PC,SC, TC;
-	cout<< "Ingrese el primer coeficiente" << endl;
+	int PC,SC,TC;
+	double Dis,X1,X2,X,I1,I2;
+	cout<< "Ingrese el primer coeficiente: " ;
 	cin>> PC;
-	cout<< "Ingrese el segundo coeficiente" << endl;
+	cout<< "Ingrese el segundo coeficiente: " ;
 	cin>> SC;
-	cout<< "Ingrese el tercer coeficiente" << endl;
+	cout<< "Ingrese el tercer coeficiente: " ;
 	cin>> TC;
 	Dis=pow(SC,2)-4*PC*TC;
-	X1= (-SC-sqrt(Dis))/2*a;
+	X1= (-SC+sqrt(Dis))/(2*PC);
+	X2= (-SC-sqrt(Dis))/(2*PC);
+	X= -SC/(2*PC);
+	I1= -SC/(2*PC);
+    I2= sqrt(-Dis)/(2*PC);
+        
+	if (Dis>0){
+		cout<< "Presenta raices reales diferentes: " << endl;
+		cout<< "La primera raiz es: "<< X1<<endl;
+		cout<< "La segunda raiz es: " << X2 <<endl;
+    }else if (Dis==0) {
+    	cout<< "Presenta raices reales iguales: " << X << endl;
+	}else{
+        cout << "Presenta raices complejas: "<<endl;
+		cout<< "La primera raiz es: " << I1<<"+"<<I2<<"i"<<endl;
+        cout << "La segunda raiz es: "<<I1<<"-"<<I2<<"i"<<endl;
+	}
 	
-	cout<< "Las raices de la ecuacion son: "<< endl;
+	return 0;
 }
