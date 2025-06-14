ceb223e935aa262b05840c002e0ebbf051af129b
Madi <mayradiazchambilla327@gmail.com>
2025-06-14 13:45:54 -0500

    Agregando avance del ejercicio 7 donde ya se declararon sus variables a utilizar en el juego de dados

---
+++ Problema7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+    void DADO_1(){
+    }
+    
+    void DADO_2(){
+	}
+	
+    void DADO_3(){
+	}
+	
+    void DADO_4(){
+	}
+	
+    void DADO_5(){
+	}
+	
+    void DADO_6(){
+	}
+	
+int main() {
+    srand(time(0));
+    int Jugador1=0;
+    int jugador2=0;
+    
+	cout << "Juego de dados " << endl;
+	cout<< "START GAME"<< endl;
+	cout<<"Presiona enter para comenzar el juego: ";
+	cin.get();
+	system("cls");	
