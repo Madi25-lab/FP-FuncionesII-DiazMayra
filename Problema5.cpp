7c289416cc2970a31694df2328759370c934b9b8
Madi <mayradiazchambilla327@gmail.com>
2025-06-14 20:41:46 -0500

    Agregando version final del ejercicio 5, para encontrar raices donde se integra una funcion para su resolucion

--- Problema5.cpp
+++ Problema5.cpp
@@ -3,18 +3,56 @@
 
 using namespace std;
 
-int main (){
-	void calcularRaices(double a,double b,double c,double &R1,double &R2,bool &Real){
+	void calcularRaices(double a,double b,double c,double &R1,double &R2,bool &R){
     double discriminante = pow(b, 2) - 4 * a * c;
 
     if(discriminante >= 0){
-        Real=true;
-        resul1=(-b + sqrt(discriminante)) / (2*a);
-        resul2=(-b - sqrt(discriminante)) / (2*a);
+        R=true;
+        R1=(-b + sqrt(discriminante)) / (2*a);
+        R2=(-b - sqrt(discriminante)) / (2*a);
     }else{
-    	Real=false;
-        resul1= -b / (2*a);
-        resul2= sqrt(-discriminante) / (2*a);
+    	R=false;
+        R1= -b / (2*a);
+        R2= sqrt(-discriminante) / (2*a);
     }
 }
+
+int main(){
+    double a,b,c;
+    double discriminante,R1,R2;
+    double Real, Imag;
+    bool R;
+
+    cout << "==========================================================" << endl;
+	cout << "              Calcular Raices Cuadraticas" << endl;
+	cout << "==========================================================\n" << endl;
+    cout<<"Ingrese el primer coeficiente: ";
+    cin>>a;
+    cout<<"Ingrese el segundo coeficiente: ";
+    cin>>b;
+    cout<<"Ingrese el tercer coeficiente: ";
+    cin>>c;
+    cout<<endl;
+
+    calcularRaices(a,b,c,R1,R2,R);
+
+    if (R){
+        if (R1 == R2){
+        cout<< "Presenta raices reales iguales: " << R1<< endl;
+        	
+        }else{
+        cout<< "Presenta raices reales diferentes: " << endl;
+	    cout<< "La primera raiz es: "<< R1<<endl;
+		cout<< "La segunda raiz es: " << R2 <<endl;
+        }
+	
+	}else{
+        Real= R1;
+        Imag= R2;
+        cout << "Presenta raices complejas: " << endl;
+        cout << "La primera raiz es: "<<Real<<" + "<<Imag<<"i"<<endl;
+        cout << "La segunda raiz es:"<<Real<<" - "<<Imag<<"i"<<endl;
+    }
+	
+	return 0;
 }
